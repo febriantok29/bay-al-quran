a4e76170669d962fd8bd4f0caf20855de48fdf95
Febrianto Kabisatullah <febriantok29>
2019-10-05 12:43:29 +0700

    Add bay colors resources

---
+++ lib/app/resources/BayColors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/painting.dart';
+
+class BayColors {
+  BayColors._();
+
+  static BayColors _instance;
+
+  factory BayColors() {
+    if (_instance == null) _instance = BayColors._();
+    return _instance;
+  }
+
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFF787878);
+}
