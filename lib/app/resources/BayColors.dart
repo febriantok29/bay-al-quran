33a2ef84ff441e007b9c2ea62218c7d765491343
Febrianto Kabisatullah <febriantok29>
2019-10-17 18:10:57 +0700

    Modify bay colors

--- lib/app/resources/BayColors.dart
+++ lib/app/resources/BayColors.dart
@@ -12,5 +12,6 @@ class BayColors {
 
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
-  static const Color grey = Color(0xFF787878);
+  static const Color grey = Color(0xFFCDCDCD);
+  static const Color blue = Color(0xFF57A5FF);
 }
