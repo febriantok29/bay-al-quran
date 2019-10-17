1946eac0cf9417b25c78bb14947d1551464fdb5e
Febrianto Kabisatullah <febriantok29>
2019-10-17 18:19:40 +0700

    Modify home page

--- lib/app/pages/Home.dart
+++ lib/app/pages/Home.dart
@@ -1,3 +1,6 @@
+import 'package:bay_al_quran/app/models/Surah.dart';
+import 'package:bay_al_quran/app/services/SurahService.dart';
+import 'package:bay_al_quran/app/ui-items/SurahCard.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -9,39 +12,62 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final _surah = SurahService().getAll();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return WillPopScope(
+      onWillPop: _handleBackButton,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
+        body: _buildContent(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Future<bool> _handleBackButton() async {
+    var result = await showDialog<bool>(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: Text('Want to exit?'),
+              actions: <Widget>[
+                FlatButton(
+                    onPressed: () => Navigator.of(context).pop(true),
+                    child: Text('Yes')),
+                FlatButton(
+                    onPressed: () => Navigator.of(context).pop(false),
+                    child: Text('No')),
+              ],
+            ));
+
+    return result == null ? false : result;
+  }
+
+  Widget _buildContent() {
+    return Center(
+      child: FutureBuilder<List<Surah>>(
+          future: _surah,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            if (snapshot.hasData) {
+              final data = snapshot.data;
+              return ListView(
+                children: data
+                    .map((surah) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: SurahCard(surah: surah),
+                        ))
+                    .toList(),
+              );
+            }
+
+            return CircularProgressIndicator();
+          }),
     );
   }
 }
