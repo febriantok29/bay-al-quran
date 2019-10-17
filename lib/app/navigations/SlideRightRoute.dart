1fa858f71a8aaf1418f35d102ca8061078807d72
Febrianto Kabisatullah <febriantok29>
2019-10-17 18:16:37 +0700

    Add page route builder for navigation animation

---
+++ lib/app/navigations/SlideRightRoute.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/widgets.dart';
+
+class SlideRightRoute extends PageRouteBuilder {
+  final WidgetBuilder builder;
+
+  SlideRightRoute({this.builder})
+      : super(
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              builder(context),
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              SlideTransition(
+            position: Tween<Offset>(
+              begin: const Offset(1.0, 0.0),
+              end: Offset.zero,
+            ).animate(animation),
+            child: child,
+          ),
+        );
+}
